a3c6bee9fdc7a7811966972e1b54e3c021a71775
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-09 13:04:57 -0500

    refactor downsample<float,4,2>

--- simd_helpers/udsample.hpp
+++ simd_helpers/udsample.hpp
@@ -22,11 +22,17 @@ namespace simd_helpers {
 // Note: the downsampling kernel does not divide the result by N !!
 
 
+inline __m128 _kernel128_downsample2(__m128 x, __m128 y)
+{
+    __m128 u = _mm_shuffle_ps(x, y, 0x88);   // [v0 v2 v4 v6],  0x88 = (2020)_4
+    __m128 v = _mm_shuffle_ps(x, y, 0xdd);   // [v1 v3 v5 v7],  0xdd = (3131)_4
+    return u + v;
+
+}
+
 inline simd_t<float,4> downsample(const simd_ntuple<float,4,2> &t)
 {
-    simd_t<float,4> u = _mm_shuffle_ps(t.extract<0>().x, t.extract<1>().x, 0x88);   // [v0 v2 v4 v6],  0x88 = (2020)_4
-    simd_t<float,4> v = _mm_shuffle_ps(t.extract<0>().x, t.extract<1>().x, 0xdd);   // [v1 v3 v5 v7],  0xdd = (3131)_4
-    return u + v;
+    return _kernel128_downsample2(t.extract<0>().x, t.extract<1>().x);
 }
 
 
