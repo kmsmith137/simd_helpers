1a20f8b0ac9e0304de2c55006ee17e00f5d731af
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-01-01 17:41:25 -0500

    start systematic unit testing: load/extract/store/constructors

--- test-basics.cpp
+++ test-basics.cpp
@@ -4,6 +4,44 @@ using namespace std;
 using namespace simd_helpers;
 
 
+template<typename T, unsigned int S, unsigned int N, typename std::enable_if<(N==0),int>::type = 0>
+inline bool _check_extract(simd_t<T,S> x, const T *v) { return true; }
+
+template<typename T, unsigned int S, unsigned int N, typename std::enable_if<(N>0),int>::type = 0>
+inline bool _check_extract(simd_t<T,S> x, const T *v) { return _check_extract<T,S,N-1>(x,v) && (x.template extract<N-1>() == v[N-1]); }
+
+
+template<typename T, unsigned int S>
+inline void test_load_store_extract(std::mt19937 &rng)
+{
+    vector<T> v = uniform_randvec<T> (rng, S, -1000, 1000);
+    simd_t<T,S> x = simd_t<T,S>::loadu(&v[0]);
+
+    bool extract_ok = _check_extract<T,S,S>(x, &v[0]);
+    assert(extract_ok);
+
+    vector<T> w = vectorize(x);
+    assert(strictly_equal(v,w));   // tests simd_t<T,S>::storeu()
+}
+
+
+template<typename T, unsigned int S>
+inline void test_constructors(std::mt19937 &rng)
+{
+    T t = uniform_randvec<T> (rng, 1, -1000, 1000)[0];
+
+    vector<T> vt = vectorize(simd_t<T,S> (t));
+    vector<T> v0 = vectorize(simd_t<T,S>::zero());
+    vector<T> vr = vectorize(simd_t<T,S>::range());
+
+    for (unsigned int s = 0; s < S; s++) {
+	assert(vt[s] == t);
+	assert(v0[s] == 0);
+	assert(vr[s] == s);
+    }
+}
+
+
 // Tests constructor (simd_t<T,S>, simd_t<T,S>) -> simd_t<T,2*S>
 template<typename T, unsigned int S>
 inline void test_merging_constructor(std::mt19937 &rng) 
@@ -22,6 +60,7 @@ inline void test_merging_constructor(std::mt19937 &rng)
     }
 };
 
+
 template<typename T, unsigned int S>
 inline void test_abs(std::mt19937 &rng)
 {
@@ -36,19 +75,39 @@ inline void test_abs(std::mt19937 &rng)
 }
 
 
+// -------------------------------------------------------------------------------------------------
+
+
+// Runs unit tests which are defined for every pair (T,S)
+template<typename T, unsigned int S>
+inline void test_all_TS(std::mt19937 &rng)
+{
+    test_load_store_extract<T,S>(rng);
+    test_constructors<T,S>(rng);
+    test_abs<T,S>(rng);
+}
+
+
+// Runs unit tests which are defined for every T
+template<typename T>
+inline void test_all_T(std::mt19937 &rng)
+{
+    constexpr unsigned int S = 16 / sizeof(T);
+
+    test_all_TS<T,S> (rng);
+    test_all_TS<T,2*S> (rng);
+    test_merging_constructor<T,S> (rng);
+}
+
+
 int main(int argc, char **argv)
 {
     std::random_device rd;
     std::mt19937 rng(rd());
 
     for (int iter = 0; iter < 1000; iter++) {
-	test_merging_constructor<int,4> (rng);
-	test_merging_constructor<float,4> (rng);
-
-	test_abs<int,4> (rng);
-	test_abs<int,8> (rng);
-	test_abs<float,4> (rng);
-	test_abs<float,8> (rng);
+	test_all_T<int>(rng);
+	test_all_T<float>(rng);
     }
 
     cout << "test-basics: pass\n";
