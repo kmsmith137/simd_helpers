7ce20c6fb4ad0d73a893de44c4f5c0952ce8beda
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-30 16:45:27 -0400

    more downsampling improvements

--- simd_helpers/downsample.hpp
+++ simd_helpers/downsample.hpp
@@ -16,9 +16,15 @@ namespace simd_helpers {
 #endif
 
 
+// This file contains some "weird boilerplate" designed to provide a streamlined
+// interface while working around C++ restrictions on partial template specialization.
+// The "_simd_downsampler" class is part of the weird boilerplate!
+template<typename T, int S, int D, typename Op> struct _simd_downsampler;
+
+
 // -------------------------------------------------------------------------------------------------
 //
-// Binary operations used in downsampling kernels.
+// Downsampling API defined in this file.
 
 
 template<typename T, int S> struct simd_add {
@@ -26,80 +32,97 @@ template<typename T, int S> struct simd_add {
 };
 
 
-// -------------------------------------------------------------------------------------------------
-//
-// simd_downsampler.
-
 template<typename T, int S, int D, typename Op = simd_add<T,S> >
 struct simd_downsampler { 
+    template<int N> inline void put(simd_t<T,S> x);
     inline simd_t<T,S> get();
 
-    template<int N>
-    inline void put(simd_t<T,S> x);
+    // Weird boilerplate
+    _simd_downsampler<T,S,D,Op> _s;
 };
 
 
-template<typename Tds, typename T, int S, int N, typename std::enable_if<(N==0),int>::type = 0>
-inline void _simd_put(Tds &ds, const simd_ntuple<T,S,N> &x) { }
+template<typename T, int S, typename Op = simd_add<T,S>, int N>
+inline simd_t<T,S> simd_downsample(const simd_ntuple<T,S,N> &x);
 
-template<typename Tds, typename T, int S, int N, typename std::enable_if<(N>0),int>::type = 0>
-inline void _simd_put(Tds &ds, const simd_ntuple<T,S,N> &x)
+
+// -------------------------------------------------------------------------------------------------
+//
+// Weird boilerplate
+
+
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==0),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put0(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==1),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put1(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==2),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put2(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==3),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put3(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==4),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put4(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==5),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put5(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==6),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put6(x); }
+template<int N, typename Td, typename Ts, typename std::enable_if<(N==7),int>::type=0> inline void _simd_ds_put(Td &d, Ts x) { d.put7(x); }
+
+template<typename T, int S, int D, typename Op> 
+template<int N>
+inline void simd_downsampler<T,S,D,Op>::put(simd_t<T,S> x) 
+{ 
+    _simd_ds_put<N> (_s, x); 
+}
+
+template<typename T, int S, int D, typename Op> 
+inline simd_t<T,S> simd_downsampler<T,S,D,Op>::get()
 {
-    _simd_put(ds, x.v);
-    ds.put<N-1> (x);
+    return _s.get();
 }
 
 
-template<typename T, int S, int N>
+template<typename Td, typename T, int S, int N, typename std::enable_if<(N==0),int>::type=0>
+inline void _simd_ds_mput(Td &d, const simd_ntuple<T,S,N> &x) { }
+
+template<typename Td, typename T, int S, int N, typename std::enable_if<(N>0),int>::type=0>
+inline void _simd_ds_mput(Td &d, const simd_ntuple<T,S,N> &x) 
+{ 
+    _simd_ds_mput(d, x.v);
+    _simd_ds_put<N-1> (d, x.x);
+}
+
+template<typename T, int S, typename Op, int N>
 inline simd_t<T,S> simd_downsample(const simd_ntuple<T,S,N> &x)
 {
-    simd_downsampler<T,S,N> ds;
-    _simd_put(ds, x);
+    _simd_downsampler<T,S,N,Op> ds;
+    _simd_ds_mput(ds, x);
     return ds.get();
 }
 
-
 // -------------------------------------------------------------------------------------------------
 
 
-template<>
-struct simd_downsampler<float,8,2,simd_add<float,8> > {
+template<typename Op>
+struct _simd_downsampler<float,8,2,Op>
+{
     simd_t<float,8> a;
 
+    inline void put0(simd_t<float,8> a_) { a = a_; }
+
+    inline void put1(simd_t<float,8> b) 
+    { 
+	// Similar to _kernel128_downsample2(), but with an extra 64-bit swap
+	// which in theory helps the subsequent permute/blend block pipeline.
+	// (Not sure if this actually helps!)
+
+	simd_t<float,8> u = _mm256_shuffle_ps(b.x, a.x, 0x88);   // [b0 b2 a0 a2],  0x88 = (2020)_4
+	simd_t<float,8> v = _mm256_shuffle_ps(b.x, a.x, 0xdd);   // [b1 b3 a1 a3],  0xdd = (3131)_4
+	simd_t<float,8> w = Op::op(u,v);
+
+	// Now we have a vector [ w2 w0 w3 w1 ], where w_i is 64 bits
+	// and we want to rearrange to [ w0 w1 w2 w3 ].
+	
+	__m256 x = _mm256_permute_ps(w.x, 0x4e);          // [ w0 w2 w1 w3 ],  0x4e = (1032)_4
+	__m256 y = _mm256_permute2f128_ps(w.x, w.x, 0x01);  // [ w3 w1 w2 w0 ]
+	
+	a = _mm256_blend_ps(x, y, 0x3c);   // (00111100)_2
+    }
+
     inline simd_t<float,8> get() { return a; }
-    
-    template<int N>
-    inline void put(simd_t<float,8> x);
 };
-
-
-template<>
-inline void simd_downsampler<float,8,2,simd_add<float,8> >::put<0> (simd_t<float,8> a_)
-{
-    a = a_;
-}
-
-#if 0
-template<typename Op>
-inline void simd_downsampler<float,8,2,Op>::put<1> (simd_t<float,8> b)
-{
-    // Similar to _kernel128_downsample2(), but with an extra 64-bit swap
-    // which in theory helps the subsequent permute/blend block pipeline.
-    // (Not sure if this actually helps!)
-
-    simd_t<float,8> u = _mm256_shuffle_ps(b.x, a.x, 0x88);   // [b0 b2 a0 a2],  0x88 = (2020)_4
-    simd_t<float,8> v = _mm256_shuffle_ps(b.x, a.x, 0xdd);   // [b1 b3 a1 a3],  0xdd = (3131)_4
-    simd_t<float,8> w = Op::op(u,v);
-
-    // Now we have a vector [ w2 w0 w3 w1 ], where w_i is 64 bits
-    // and we want to rearrange to [ w0 w1 w2 w3 ].
-
-    __m256 x = _mm256_permute_ps(w.x, 0x4e);          // [ w0 w2 w1 w3 ],  0x4e = (1032)_4
-    __m256 y = _mm256_permute2f128_ps(w.x, w.x, 0x01);  // [ w3 w1 w2 w0 ]
-
-    a = _mm256_blend_ps(x, y, 0x3c);   // (00111100)_2
-}
-#endif
 
 
 }  // namespace simd_helpers
