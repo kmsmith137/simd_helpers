46c4b76e7f446dbf0053a652d1a705415e43e490
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2020-01-11 15:57:13 -0500

    small LegacyUninstaller interface change

--- legacy_uninstall.py
+++ legacy_uninstall.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 
-import os
-import sys
 import build_helpers
 
 # If called recursively in superbuild, a global persistent LegacyUninstaller will be returned.
 lu = build_helpers.get_global_legacy_uninstaller()
 
-incdir = os.path.join(os.environ['HOME'], 'include')
-lu.add_glob(incdir, 'simd_helpers.hpp')
-lu.add_glob(incdir, 'simd_helpers/*.hpp')
-lu.add_glob(incdir, 'simd_helpers')
+lu.uninstall_headers('simd_helpers.hpp')
+lu.uninstall_headers('simd_helpers/*.hpp')
+lu.uninstall_headers('simd_helpers/')
 
 # If called recursively in superbuild, run() will not be called here.
 if __name__ == '__main__':
